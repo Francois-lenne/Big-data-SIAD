5ed539c785c6b96bd5723c0b89be97424536c536
Ronan76 <116274154+Ronan76@users.noreply.github.com>
2023-02-27 09:30:25 +0100

    rangement

--- extension/api/test2.py
+++
@@ -1,8 +0,0 @@
-from model import *
-import pandas as pd
-
-tweet = "Je suis le tweet à tester, je suis là pour le modèle."
-
-df_tweet = pd.DataFrame({'id': [1], 'keyword': [''], 'location': ['New York'], 'text': ['un test pour notre modèle']})
-model_test = TweetCheckerModel()
-print(model_test.submit(df_tweet))
